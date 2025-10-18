45b7bf32b01133479b4f7267ec93907f135ac17c
huong <59523300+imhuog@users.noreply.github.com>
2025-10-18 16:46:45 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
+app.options("*", cors()); // ✅ xử lý preflight request cho tất cả route
 
 app.use(express.json());
 
